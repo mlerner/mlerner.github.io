88e8f32b7310fd24f89a329d2bfa8287db1293a0
mlern <mlerner789@gmail.com>
2021-03-29 13:12:16 -0700

    Noria draft

--- _plugins/marginfigure.rb
+++ _plugins/marginfigure.rb
@@ -1,26 +1,25 @@
 module Jekyll
-    class RenderMarginFigureTag < Liquid::Tag
+  class RenderMarginFigureTag < Liquid::Tag
+    require "shellwords"
 
-          require "shellwords"
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.shellsplit
+    end
 
-              def initialize(tag_name, text, tokens)
-                      super
-                            @text = text.shellsplit
-                                end
-
-                  def render(context)
-                          baseurl = context.registers[:site].config['baseurl']
-                                if @text[1].start_with?('http://', 'https://', '//')
-                                          "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>"+
-                                                    "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>"+
-                                                            "<span class='marginnote'><img class='fullwidth' src='#{@text[1]}'/><br>#{@text[2]}</span>"
-                                                else
-                                                          "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>"+
-                                                                  "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>"+
-                                                                            "<span class='marginnote'><img class='fullwidth' src='#{baseurl}/#{@text[1]}'/><br>#{@text[2]}</span>"
-                                                                                  end
-                                    end
-                    end
+    def render(context)
+      baseurl = context.registers[:site].config["baseurl"]
+      if @text[1].start_with?("http://", "https://", "//")
+        "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>" +
+          "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>" +
+          "<span class='marginnote'><img class='fullwidth' src='#{@text[1]}'/><br>#{@text[2]}</span>"
+      else
+        "<label for='#{@text[0]}' class='margin-toggle'>&#8853;</label>" +
+          "<input type='checkbox' id='#{@text[0]}' class='margin-toggle'/>" +
+          "<span class='marginnote'><img class='fullwidth' src='#{baseurl}/#{@text[1]}'/><br>#{@text[2]}</span>"
+      end
+    end
+  end
 end
 
-Liquid::Template.register_tag('marginfigure', Jekyll::RenderMarginFigureTag)
+Liquid::Template.register_tag("marginfigure", Jekyll::RenderMarginFigureTag)
